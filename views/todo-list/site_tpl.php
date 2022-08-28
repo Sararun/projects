0e7cc403a17e7f052d337568311f2fe88be9a668
Sararun <sarosekvlad12@gmail.com>
2022-08-28 18:01:00 +0700

    убрал лишнюю разметку

--- views/todo-list/site_tpl.php
+++ views/todo-list/site_tpl.php
@@ -26,7 +26,7 @@
                     <td>
                         <input id="is_active" name="is_active" type="checkbox" class="form-check-input"
                                 <?php if (!empty($value['executed'])): ?>
-                                    checked disabled<?php else: ?> disabled <?php
+                                    checked  <?php
                                 endif;?>>
                     </td>
                     <td>
