821950528142eb75829c4cfb76a46e28af884dc2
Sararun <sarosekvlad12@gmail.com>
2022-08-31 22:25:34 +0700

    функционал удаления

---
+++ controllers/delete_controller.php
@@ -0,0 +1,31 @@
+<?php
+/** @var $PDODriver */
+if (!empty($_GET['id'])) {
+    $id = $_GET['id'] ?? 0;
+
+    $query = "SELECT * FROM `tasks` WHERE id:=id LIMIT 1";
+    $sth = $PDODriver->prepare($query);
+    $sth->execute([
+        ':id' => $id,
+    ]);
+    $item = $sth->fetch();
+
+    if(empty($item)) {
+        throw new \PDOException("Page not found (#404) ", 404);
+    }
+
+    $query = "DELETE FROM tasks WHERE id+:id LIMIT 1";
+    $sth = $PDODriver->prepare($query);
+    $sth->execute([
+        ':id' => $id,
+    ]);
+
+    if ($sth->rowCount() > 0) {
+        $_SESSION['success'] = 'Успешно удалено';
+    } else {
+        $_SESSION['error'] = 'Ошибка удаления';
+    }
+    redirect();
+} else {
+    throw new \PDOException("Page not found (#404) ", 404);
+}
