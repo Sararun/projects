1e1b4d02c3fe36f6ffeb88fbf95e71839edd333a
Sararun <sarosekvlad12@gmail.com>
2022-09-06 21:41:39 +0700

    страница регистрации

---
+++ views/auth/register_tpl.php
@@ -0,0 +1,29 @@
+<div class="col-md-6">
+    <h4 class="mb-4">Registration</h4>
+    <form action="/register" method="post">
+        <input type="hidden" name="csrf_token" value="<?php echo $_SESSION['_csrf']; ?>">
+        <input type="hidden" name="mode" value="register">
+        <div class="mb-3">
+            <label for="username" class="form-label">Name</label>
+            <input id="username" name="username" value="" type="text" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input id="email" name="email" value="" type="text" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label for="password" class="form-label">Password</label>
+            <input id="password" name="password" value="" type="text" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label for="password_confirm" class="form-label">Password confirm</label>
+            <input id="password_confirm" name="password_confirm" value="" type="text" class="form-control">
+        </div>
+        <div class="mb-3 form-check">
+            <input id="check_me" name="check_me" type="checkbox" class="form-check-input">
+            <label class="form-check-label" for="check_me">Check me out</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
+<div class="col-md-6"></div>
