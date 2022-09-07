697b88aa6dfca68bf865e50de921785b38693676
Sararun <sarosekvlad12@gmail.com>
2022-09-08 01:06:46 +0700

    добавил больше функционала суперпользователю

--- controllers/edit_controller.php
+++ controllers/edit_controller.php
@@ -3,17 +3,33 @@
 /** @var $controller */
 $id = $_GET['id'] ?? 0;
 
-$query = "SELECT * FROM `tasks` WHERE id=:id LIMIT 1";
+$where = 'WHERE id=:id';
+$params = [':id' => $taskId];
+
+//Если user не пустой или user обычный, то присваиваем id
+if (!empty($_SESSION['user']) && $_SESSION['user']['role'] == 2) {
+    $userId = $_SESSION['user']['id'];
+    $params[':user_id'] = $userId;
+    $where .= "AND user_id=:user_id";
+}
+
+$query = "SELECT * FROM `tasks` {$where} LIMIT 1";
 $sth = $PDODriver->prepare($query);
-$sth->execute([
-    ':id' => $id,
-]);
+$sth->execute($params);
 $item = $sth->fetch();
 
 if (empty($item)) {
     throw new \PDOException("Page not found (#404) ", 404);
 }
 
+//Если user не пустой или это админ, то вывод пользователей
+if (!empty($_SESSION['user']) && ($_SESSION['user']['role'] == 1)) {
+    $query = "SELECT id, username FROM users ORDER BY id DESC";
+    $sth->execute();
+    $users = $sth->fetchAll();
+}
+
 $content = render($controller, [
     'item' => $item,
+    'users' => $users ?? [],
 ]);
