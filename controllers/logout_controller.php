c4d6e3ed05a11e4a1d59dfbe615178a158aaaf14
Sararun <sarosekvlad12@gmail.com>
2022-09-13 23:46:44 +0700

    Убрал логику проверки на выход, и редирект на логин

--- controllers/logout_controller.php
+++ controllers/logout_controller.php
@@ -1,10 +1,7 @@
 <?php
 /** @var $PDODriver */
-/** @var $controller */
 
 //Выход из профиля
-if (!empty($_GET['out']) && ($_GET['out'] == 1)) {
-    $_SESSION['user'] = [];
-    unset($_SESSION['user']);
-    redirect();
-}
+$_SESSION['user'] = [];
+unset($_SESSION['user']);
+redirect('/');
