b360285d428164700fc7e44778d247f6baf2fab7
Sararun <sarosekvlad12@gmail.com>
2022-09-15 01:58:19 +0700

    Добавил поддержку пагинатора и дедлайна

--- views/tasks/index_tpl.php
+++ views/tasks/index_tpl.php
@@ -15,10 +15,6 @@
                         <form action="/" method="get" class="row g-3">
                             <input type="hidden" name="filter" value="1">
                             <div class="col-md-4">
-                                <label for="username" class="form-label">User</label>
-                                <input id="username" name="username" value="" type="text" class="form-control">
-                            </div>
-                            <div class="col-md-4">
                                 <label for="title" class="form-label">Title</label>
                                 <input id="title" name="title" value="" type="text" class="form-control">
                             </div>
@@ -54,6 +50,7 @@
                 </div>
             </div>
         </div>
+
         <hr>
         <div class="table-responsive">
             <table class="table">
@@ -76,10 +73,10 @@
                         && ($value['executed'] != 1); ?>
                     <tr id="tr<?php echo $value['id']; ?>"
                         class="<?php if ($value['executed']): ?>table-success<?php endif; ?>
-                    <?php if ($isDeadline): ?>table-danger<?php endif; ?>">
+                        <?php if ($isDeadline && ($value['executed'] != 1)): ?>table-danger<?php endif; ?>">
                         <th scope="row"><?php echo $value['id']; ?></th>
                         <td>
-                            <?php echo $value['username']; ?>
+                            <?php echo $value['username']; ?> <?php var_dump($isDeadline);?>
                             <?php echo $value['executed']; ?>
                         </td>
                         <td>
@@ -106,7 +103,9 @@
                             <?php endif; ?>
                         </td>
                         <td>
-                            <?php if ($_SESSION['user']['role'] == 1): ?>
+                            <?php if ($isDeadline && $_SESSION['user']['role'] == 1): ?>
+                                <a class="btn btn-primary" href="/edit?id=<?php echo $value['id']; ?>" role="button">Edit</a>
+                            <?php elseif (!$isDeadline): ?>
                                 <a class="btn btn-primary" href="/edit?id=<?php echo $value['id']; ?>" role="button">Edit</a>
                             <?php endif; ?>
 
@@ -119,46 +118,59 @@
                 </tbody>
             </table>
         </div>
+
+        <nav aria-label="Page navigation example">
+            <ul class="pagination justify-content-end">
+                <?php echo $paginator; ?>
+            </ul>
+        </nav>
+
     <?php else: ?>
-        <div class="alert alert-danger" role="alert">
-            Позапросу ничего не найдено.
+        <?php if (!empty($_GET['search'])): ?>
+            <div class="alert alert-danger" role="alert">
+                По вашему запросу <b>"<?php echo $_GET['search']; ?>"</b> ничего не найдено.
+            </div>
+            <a class="btn btn-secondary" href="/" role="button">Сброс</a>
+        <?php else: ?>
+            <p>
+                Заданий не найдено.
+            </p>
+        <?php endif ?>
+    <?php endif; ?>
+
+    <h1>Add to task</h1>
+    <form action="/store" method="post">
+        <input type="hidden" name="csrf_token" value="<?php echo $_SESSION['_csrf']; ?>">
+        <div class="mb-3">
+            <label for="title" class="form-label">Title</label>
+            <input id="title" name="title"
+                   value="<?php if (!empty($_SESSION['data']['title'])): ?><?php echo $_SESSION['data']['title']; ?><?php endif; ?>"
+                   type="text" class="form-control">
         </div>
-        <a class="btn btn-secondary" href="/" role="button">Сброс</a>
-    <?php endif; ?>
-    <?php if ($_SESSION['user']['role'] == 1): ?>
-        <h1>Add to task</h1>
-        <form action="/store" method="post">
-            <input type="hidden" name="csrf_token" value="<?php echo $_SESSION['_csrf']; ?>">
+        <?php if (!empty($_SESSION['user']) && ($_SESSION['user']['role'] == 1)): ?>
             <div class="mb-3">
-                <label for="title" class="form-label">Title</label>
-                <input id="title" name="title"
-                       value="<?php if (!empty($_SESSION['data']['title'])): ?><?php echo $_SESSION['data']['title']; ?><?php endif; ?>"
-                       type="text" class="form-control">
+                <label for="title" class="form-label">Users</label>
+                <select name="user_id" class="form-select" aria-label="Default select example">
+                    <?php /** @var array $users */?>
+                    <?php foreach ($users as $value): ?>
+                        <option value="<?php echo $value['id']; ?>">
+                            <?php echo $value['username']; ?>
+                        </option>
+                    <?php endforeach; ?>
+                </select>
             </div>
-            <?php if (!empty($_SESSION['user']) && ($_SESSION['user']['role'] == 1)): ?>
-                <div class="mb-3">
-                    <label for="title" class="form-label">Users</label>
-                    <select name="user_id" class="form-select" aria-label="Default select example">
-                        <?php /** @var array $users */?>
-                        <?php foreach ($users as $value): ?>
-                            <option value="<?php echo $value['id']; ?>">
-                                <?php echo $value['username']; ?>
-                            </option>
-                        <?php endforeach; ?>
-                    </select>
-                </div>
-            <?php endif; ?>
-            <div class="mb-3">
-                <label for="description" class="form-label">Description</label>
-                <textarea id="description" name="description" class="form-control" rows="3"><?php if (!empty($_SESSION['data']['description'])): ?><?php echo $_SESSION['data']['description']; ?><?php endif; ?></textarea>
-            </div>
-            <div class="mb-3">
-                <label for="deadline" class="form-label">Deadline</label>
-                <input id="deadline" name="deadline" type="datetime-local" class="form-control">
-            </div>
-            <input type="hidden" name="mode" value="create">
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
-    <?php endif; ?>
+        <?php endif; ?>
+        <div class="mb-3">
+            <label for="description" class="form-label">Description</label>
+            <textarea id="description" name="description" class="form-control"
+                      rows="3"><?php if (!empty($_SESSION['data']['description'])): ?><?php echo $_SESSION['data']['description']; ?><?php endif; ?></textarea>
+        </div>
+        <div class="mb-3">
+            <label for="deadline" class="form-label">Deadline</label>
+            <input id="deadline" name="deadline" type="datetime-local" class="form-control">
+        </div>
+        <input type="hidden" name="mode" value="create">
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
 </div>
 <?php unset($_SESSION['data']); ?>
