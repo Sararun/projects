3be8926cf86bef43b836d13577e1874cb087a227
Sararun <sarosekvlad12@gmail.com>
2022-09-15 02:01:58 +0700

    Изменил индекс с учётом существующих функций, также изменил названия некоторых переменных

--- index.php
+++ index.php
@@ -17,47 +17,44 @@ try {
     //разбирает URL и возвращает его компоненты
     //[path] => / [query] => id=1
     $partsPath = parse_url($url);
-    $stringPath = $partsPath['path'] ?? '';
+    $currentPath = $partsPath['path'] ?? '';
 
     //подключаме роуты
     if (empty($_SESSION['user'])) {
-        $controller = 'login';
+        $currentController = 'login';
     } else {
         $routes = require __DIR__ . '/config/routes.php';
-        $controller = '';
+        $currentController = '';
         foreach ($routes as $route) {
             //проверяем на совпадение урл с роутами
-            if (preg_match($route['url'], $stringPath)) {
+            if (preg_match($route['url'], $currentPath)) {
                 //если совпадение найдено прерываем цикл
                 //и записываем имя контроллера в переменную
-                $controller = $route['controller'];
+                $currentController = $route['controller'];
                 break;
             }
         }
     }
+
     //если контроллер не найден, тогда 404 ошибка
     if (empty($controller)) {
         throw new \PDOException("Page not found (#404) ", 404);
     }
 
-    //подключаем конфиг бд и считываем массив в переменную
-    $config = require __DIR__ . '/config/db.php';
     //создаем подключение к бд
-    $PDODriver = connectDB($config);
+    $PDODriver = connectDB();
 
     //подключаем контроллер отвечающий за обработку http запроса
-    require __DIR__ . "/controllers/{$controller}_controller.php";
+    require __DIR__ . "/controllers/{$currentController}_controller.php";
     //подключаем основной шаблон клиента
     require __DIR__ . "/views/layouts/default.php";
 } catch (Exception $e) {
-    $code = $e->getCode() ?? 404;
-    //устанавливаем код ответа HTTP
-    http_response_code((int)$code);
-    //подключаем шаблон ошибки по коду
+    $code = (int)$e->getCode() ?? 404;
+    //поключаем шаблон ошибки по коду
     $ip = $_SERVER['REMOTE_ADDR'];
     $userAgent = $_SERVER['HTTP_USER_AGENT'];
     $error = date('d.m.Y H:i:s') . "|{$e->getMessage()}|$ip|$userAgent\n";
     file_put_contents(__DIR__ . '/error.txt', $error, FILE_APPEND);
-    require __DIR__ . "/views/errors/{$code}.php";
+    dispatchNotFound($code);
     die;
 }
