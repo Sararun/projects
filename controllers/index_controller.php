82220cf767c8bc998afc180e3b5ab36f6ca4c2a0
Sararun <sarosekvlad12@gmail.com>
2022-09-15 23:44:45 +0700

    убрал пробелы, убрал лишние запросы в бд

--- controllers/index_controller.php
+++ controllers/index_controller.php
@@ -2,7 +2,7 @@
 /** @var $PDODriver */
 /** @var $currentController */
 
-$whereQuery = $whereCount =  ' WHERE 1';
+$whereQuery = $whereCount = ' WHERE 1';
 $paramsQuery = [];
 
 if ($_SESSION['user']['role'] == 2) {
@@ -39,7 +39,7 @@ if ($page > $countPages) {
     $page = $countPages;
 }
 //вычитаем -1 так как берём с 0
-$limit = ($page-1) * $perPage;
+$limit = ($page - 1) * $perPage;
 $offset = $perPage;
 
 $whereQuery .= " LIMIT {$limit}, {$offset}";
@@ -53,15 +53,6 @@ if ($_SESSION['user']['role'] == 1) {
     $sth->execute();
     $users = $sth->fetchAll();
 }
-
-
-//подготавливаем запрос к выполнению
-//и возвращаем связанный с этим запросом объект
-$sth = $PDODriver->prepare($query);
-//запускаем подготовленный запрос на выполнение
-$sth->execute();
-//возвращает массив, содержащий все записи в бд
-$taskList = $sth->fetchAll();
 
 //подключаем рендер и передаем массив
 //записей в подключаемый вид для подстановке в шаблоне
